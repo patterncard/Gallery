999370d770eef397e1c8014af921df03c9f987a1
Robert Kwiatkowski <jakusienko@gmail.com>
2021-12-11 14:53:23 +0100

    small code cleanups

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QWidget ,QMainWindow, QFileDialog, QAction
-from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QSpinBox, QGridLayout, QSplitter
-from PyQt5.QtWidgets import QFrame, QTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, QSplitter
+from PyQt5.QtWidgets import QVBoxLayout, QTextEdit
 from PyQt5.QtGui import QPixmap, QKeySequence
 from PyQt5.QtCore import Qt, QEvent
 import sys
